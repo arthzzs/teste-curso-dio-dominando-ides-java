599ee0153ef034fc41b01007eece3554c728971a
arthzzs <arthurfreiittas.908@gmail.com>
2021-08-12 15:47:13 -0300

    first commit

---
+++ src/ls/com/dio/Primeiroprogrma.java
@@ -0,0 +1,17 @@
+package ls.com.dio;
+
+import ls.com.dio.model.Gato;
+
+public class Primeiroprogrma {
+
+	public static void main(String[] args) {
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Hello Word! " + (a+b));*/
+	}
+
+	}
+
